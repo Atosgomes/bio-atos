77f1733507c7adfd406e7562f6f43b9f4c0e1639
Atos Gomes <atos22dr1@hotmail.com>
2024-04-22 19:43:50 -0300

    atualização

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function tocaSom (){
+ document.querySelector('#som').play();
+};
+
+document.querySelector('.msc').onclick = tocaSom;
+document.querySelector('.msc1').onclick = tocaSom;
+document.querySelector('.msc2').onclick = tocaSom;
+document.querySelector('.msc3').onclick = tocaSom;
+document.querySelector('.msc4').onclick = tocaSom;
+document.querySelector('.msc5').onclick = tocaSom;
